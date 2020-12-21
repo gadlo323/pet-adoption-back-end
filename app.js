770833190518c2ce358d2ea251b322b43ee0c7f3
isayas <gadlo323@gmail.com>
2020-12-21 20:26:51 +0200

    feat:Auth no db

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
+const cors = require("cors");
 const port = 5000;
-
+const {
+  validateUserSignup,
+  handleValidationErrors,
+  validateUserLogin,
+} = require("./userValid");
+let users = require("./users.json");
+const { json } = require("body-parser");
 //static files(css,js)& middleware
 app.use(express.static("./public"));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("dsd");
 });
+app.post(
+  "/adoption/signup",
+  validateUserSignup,
+  handleValidationErrors,
+  (req, res) => {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      password,
+      repetPassword,
+    } = req.body;
+
+    users.push(req.body);
+    fs.writeFileSync("./users.json", JSON.stringify(users, null, 2));
+    res.send({
+      email: email,
+      name: firstName + " " + lastName,
+    });
+  }
+);
+
+app.post(
+  "/adoption/login",
+  validateUserLogin,
+  handleValidationErrors,
+  (req, res) => {
+    const { email, password } = req.body;
+
+    fs.readFile("./users.json", "utf8", function read(err, data) {
+      if (err) {
+        throw err;
+      }
+      const exists = logUser(JSON.parse(data), email, password);
+      if (exists) {
+        res.send({
+          email: exists[0].email,
+          name: exists[0].firstName + " " + exists[0].lastName,
+        });
+      } else res.send(null);
+    });
+  }
+);
+
+const logUser = (data, email, password) => {
+  const user = data.filter((item) => {
+    return item.email === email && item.password === password;
+  });
+  return user.length ? user : "";
+};
 
 //fire routes
 // app.use("/adoption", adoptionRoutes);
---
+++ userValid.js
@@ -0,0 +1,40 @@
+const { body, validationResult } = require("express-validator");
+
+const validateUserSignup = [
+  body("firstName").exists().isLength({ min: 2, max: 10 }),
+  body("lastName").exists().isLength({ min: 2, max: 12 }),
+  body("email").isEmail(),
+  body("phone").matches(/^((\+|00)\-?972?|0)(([23489]|[57]\d)\-?\d{7})$/gm),
+  body("password").matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+  ),
+  body("repeatPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Password confirmation does not match password");
+    }
+    return true;
+  }),
+];
+
+const validateUserLogin = [
+  body("email").isEmail(),
+  body("password")
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    .withMessage("password is vaild"),
+];
+
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.errors.length) {
+    next();
+  } else {
+    res.status(400).send({ errors });
+  }
+};
+
+module.exports = {
+  validateUserSignup,
+  handleValidationErrors,
+  validateUserLogin,
+};
